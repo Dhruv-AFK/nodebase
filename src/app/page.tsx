3edb4c4dd631ae91fd2ac4eb42004ab2d0370cb3
Dhruv-AFK <dhruvjha1239@gmail.com>
2025-11-10 23:07:07 +0530

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
   return ( 
     <div className="min-h-screen min-w-screen flex item-center justify-center" >
       <Button variant="outline">
-        Daba do 
+        press it 
       </Button>
     </div>
   );
